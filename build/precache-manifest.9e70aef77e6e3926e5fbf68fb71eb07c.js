e7f383bd6223cd88901bd3e3c66f9690cd2e5232
Bhavishya kumar <kr@Bhav.localdomain>
2020-09-21 04:46:00 +0530

    Second commit

---
+++ build/precache-manifest.9e70aef77e6e3926e5fbf68fb71eb07c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1279221fad0d94260ead4faa2b862f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f327d8c2ae1c7274f9b",
+    "url": "/static/js/2.53f997c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53f997c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba0b8a4e96d57b3195e",
+    "url": "/static/js/main.fa688132.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
---
+++ build/precache-manifest.a1edb77cd63315910766cbc41dce7638.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d9f2816877695e25e8f58f90352e98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f327d8c2ae1c7274f9b",
+    "url": "/static/js/2.53f997c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53f997c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd013a5a0a9853f9e17b",
+    "url": "/static/js/main.1021b9ae.chunk.js"
+  },
+  {
+    "revision": "c5e8f5ba0b491ea9966c",
+    "url": "/static/js/runtime-main.222d136d.js"
+  }
+]);
